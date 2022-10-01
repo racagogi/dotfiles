a8ca13131f740ecd02247831dc481061f4017a26
racagogi <racagogi@gmail.com>
2022-10-01 21:24:59 +0900

    new

--- dot_xmonad/executable_setup.sh
+++ dot_xmonad/executable_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ibus-daemon -drxR &
-feh --bg-scale /home/raca/.xmonad/desktop.jpg &
+feh --bg-fill /home/raca/.xmonad/desktop.jpg &
 picom -b --config /home/raca/.xmonad/picom &
 killall -q polybar &
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done 
