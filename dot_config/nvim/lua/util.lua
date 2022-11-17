ccc0f5eee6cd3d43456ee57f277d81183634b2d9
racagogi <racagogi@gmail.com>
2022-11-17 14:55:08 +0900

    comimit

--- dot_config/nvim/lua/util.lua
+++ dot_config/nvim/lua/util.lua
@@ -2,22 +2,32 @@ require('nvim-lastplace').setup {}
 require('trim').setup {}
 require("expand_expr")
 require('Comment').setup {
-  toggler = {
-    line = 'cll',
-    block = 'cbb',
-  },
-  opleader = {
-    line = 'cl',
-    block = 'cb',
-  },
-  mappings = {
-    baisic = true,
-    extra = false
-  }
+    toggler = {
+        line = 'cll',
+        block = 'cbb',
+    },
+    opleader = {
+        line = 'cl',
+        block = 'cb',
+    },
+    mappings = {
+        baisic = true,
+        extra = false
+    }
 }
-require("nvim-autopairs").setup {
-  enable_check_bracket_line = true,
-}
-require("nvim-autopairs").get_rule("'")[1].not_filetypes = { "scheme", "lisp" }
-
+require("nvim-autopairs").setup({
+    fast_wrap = {
+        map = '<M-e>',
+        chars = { '{', '[', '(', '"', "'" },
+        pattern = [=[[%'%"%)%>%]%)%}%,]]=],
+        end_key = '$',
+        keys = 'qwertyuiopzxcvbnmasdfghjkl',
+        check_comma = true,
+        highlight = 'Search',
+        highlight_grey = 'Comment'
+    },
+})
+require("nvim-autopairs").add_rule(require('nvim-autopairs.rule')("$$","$$",{"tex","latex","markdown"}))
+require("nvim-autopairs").add_rule(require('nvim-autopairs.rule')("$","$",{"tex","latex","markdown"}))
+require("nvim-autopairs").remove_rule('`')
 require('nvim-surround').setup {}
